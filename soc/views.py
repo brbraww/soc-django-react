a0a876f5420bfcba2e0ed73341e2457f16a52af2
brbr aww <legendary.grassman@gmail.com>
2020-07-29 13:55:23 +0300

    4 - templates + message list view

--- soc/views.py
+++ soc/views.py
@@ -5,10 +5,30 @@ from .models import Messages
 
 
 def home_view(request, *args, **kwargs):
-    return HttpResponse('<h1>Hello world</h1>')
+    return render(request, 'pages/home.html', context={}, status=200)
+
+
+def message_list_view(request, *args, **kwargs):
+    """
+    REST API VIEW
+    :return json data
+    """
+    qs = Messages.objects.all()
+    messages_list = [{
+        'id': x.id,
+        'content': x.content
+    } for x in qs]
+    data = {
+        'response': messages_list
+    }
+    return JsonResponse(data)
 
 
 def message_detail_view(request, message_id, *args, **kwargs):
+    """
+        REST API VIEW
+        :return json data
+        """
     data = {
         'id': message_id,
     }
